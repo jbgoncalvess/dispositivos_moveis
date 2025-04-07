e104cd575109ebc3e8705052f3c793e6806ff1cf
Jardel Gonçalves <117037558+jbgoncalvess@users.noreply.github.com>
2025-04-07 01:40:29 -0300

    Lista useState

--- exemplo_hooks/components/MeusComponentes.js
+++ exemplo_hooks/components/MeusComponentes.js
@@ -22,7 +22,47 @@ export function ExemploUseState_01 () {
   );
 
 }
+
 //==========================================================================================================
+
+
+export function ExemploUseState_02() {
+  const [nome, setNome] = useState(''); // Inicio uma variável que seu estado inicial é vazia ''.
+  const [listaNomes, setListaNomes] = useState([]); // Inicio um array que seu estado inicial é vazio [].
+
+  function adicionarNome() {
+    if (nome.trim() !== '') {
+      setListaNomes([...listaNomes, nome]); // spread operator, manipula dados em um array.
+      setNome('');                          // Neste caso nos clonamos a lista original e adicionamos o nome no final. Reinicio o nome.
+    }
+  }
+
+  function renderizarNome(index, item) { // Retorno o texto e a posição do array. Exemplo: pos 0 - Ciclano de tal, pos 1 - Fulano do mal ...
+    return <Text key={index}>{item}</Text>;
+  }
+
+  return (
+    <View style={{ alignItems: 'center', padding: 20 }}>
+      <TextInput
+        style={{ borderWidth: 2, padding: 5, marginBottom: 10, width: 200 }}
+        placeholder="Digite um nome"
+        value={nome}                    // O usuário digita o nome e com o setNome mudamos o conteúdo da variável nome, pois ela tem estado (useState).
+        onChangeText={setNome}
+      />
+      <Button title="Adicionar Nome" onPress={adicionarNome} />
+      
+    {listaNomes.map(function (item, index) { 
+      /*As chaves antes do código JS é para entrar no modo JSX, que permite executar códigos JS. Sem as chaves o React acharia que eu estou
+      escrevendo algo da estrutura visual e não a lógica de programação.*/
+      // Retorno o texto e a posição do array. Exemplo: pos 0 - Ciclano de tal, pos 1 - Fulano do mal ...
+      return <Text key={index}>{item}</Text>;
+    })}
+    </View>
+  );
+}
+
+//==========================================================================================================
+
 
 export function ExemploUseEffect_01 () {
   const [contador, setContador] = useState(0); //Esse é o padrão da definição do estado, aluno.
