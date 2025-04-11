38685efc8e1f8ac15eddc115c6bd0921112b7058
jbgonca <you@example.com>
2025-04-11 10:58:26 -0300

    Adicionando exemplos de navegação

---
+++ exemplos_navigations/screens/TelaDetalhes.js
@@ -0,0 +1,13 @@
+import {Button,View, Text } from 'react-native';
+
+// Tela detalhes sem nada que vocês não tenham visto.
+export default function Detalhes () {
+  return(
+    <View>
+      <Text style={{fontSize: 30}}>
+        Esses são os meus Detalhes!
+      </Text>
+    </View>
+  );
+}
+
